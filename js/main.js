a65c9c736e697439223baf55a7f161f83890e361
Polina <49097861+Kovriki@users.noreply.github.com>
2021-08-16 03:33:19 +0300

    53

--- js/main.js
+++ js/main.js
@@ -16,5 +16,21 @@ $(document).ready(function() {
 		});
 		return false;
 	});
+	$(function() {
+	$('.menu a, .btn_podrobnee').each(function() {
+		$(this).on('click', function(e) {
+			e.preventDefault();
+			var scrollTo = $(this).attr('href');
+			var whitespace = scrollTo == '#s1' ? 70 : 30;
+			$('html,body').animate({
+				scrollTop: $(scrollTo).offset().top - whitespace
+				}, 1000);
+			});
+		});
+	});
+
+
+// $(".slider_box").slick();
+$('.single-item').slick();
 
 });
